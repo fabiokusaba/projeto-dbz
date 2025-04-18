e942926542f6a1dc0ae9f753b865a6cb2af1b3ab
Fabio Kusaba <fabinhokb44@gmail.com>
2025-04-17 22:23:49 -0300

    add data annotations for field validation

--- ProjetoDBZ/Models/Personagem.cs
+++ ProjetoDBZ/Models/Personagem.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProjetoDBZ.Models;
 
 public class Personagem
 {
+    [Key]
     public int Id { get; set; }
+    
+    [Required(ErrorMessage = "O campo nome é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O campo nome precisa ter no máximo 50 caracteres")]
     public string Nome { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "O campo tipo é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O campo tipo precisa ter no máximo 50 caracteres")]
     public string Tipo { get; set; } = string.Empty;
 }
