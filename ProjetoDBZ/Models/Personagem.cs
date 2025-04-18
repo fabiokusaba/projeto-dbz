6a0b9885c66c7c103265f1bfe17a24e51e1c07b7
Fabio Kusaba <fabinhokb44@gmail.com>
2025-04-17 21:23:28 -0300

    add Personagem entity

---
+++ ProjetoDBZ/Models/Personagem.cs
@@ -0,0 +1,8 @@
+namespace ProjetoDBZ.Models;
+
+public class Personagem
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Tipo { get; set; } = string.Empty;
+}
